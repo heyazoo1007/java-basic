8bbba48a4a6c193a5eea4e991771b755e8d0a192
heyazoo1007 <heyazoo1007@gmail.com>
2024-01-12 13:54:26 +0900

    2024-01-12 study : 자바 기본, 추상 클래스/메서드

---
+++ src/poly/ex3_abstract/Cow.java
@@ -0,0 +1,9 @@
+package poly.ex3_abstract;
+
+public class Cow extends AbstractAnimal {
+
+    @Override
+    public void sound() {
+        System.out.println("음메");
+    }
+}
---
+++ src/poly/ex3_abstract/Dog.java
@@ -0,0 +1,9 @@
+package poly.ex3_abstract;
+
+public class Dog extends AbstractAnimal {
+
+    @Override
+    public void sound() {
+        System.out.println("멍멍");
+    }
+}
